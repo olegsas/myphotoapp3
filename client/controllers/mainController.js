57780175ebefbec8505c5f62e4dbb40164009d0d
Oleg <o.sas@aisnovations.com>
2017-01-09 12:40:20 +0300

    we add an upload form

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -1,5 +1,5 @@
-angular.module('myApp').controller('mainController', ['$scope',
-    function($scope) {
+angular.module('myApp').controller('mainController', ['$scope', '$http', 'Upload', '$timeout',
+    function($scope, $http, Upload, $timeout) {
         $scope.test = "Hello";
 
         $scope.pictures = [
@@ -7,6 +7,26 @@ angular.module('myApp').controller('mainController', ['$scope',
         'https://s-media-cache-ak0.pinimg.com/originals/30/27/d0/3027d0b550958fa1871e0a4dd475fba6.jpg',
         'http://data.whicdn.com/images/897396/large.png'
         ];
+
+        $scope.uploadPic = function(file) {
+            console.log(file);
+            file.upload = Upload.upload({
+                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                data: {file: file},
+            });
+
+            file.upload.then(function(response){
+                $timeout(function(){
+                    file.result = response.data;
+                });
+            }, function(response){
+                if(response.status >0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function(evt){
+                // Math.min is to fix IE which reports 200% sometimes
+                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            });
+        }
     }
 
 ]);
